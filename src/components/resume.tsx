d45711c3772a6e9db27e30eb443b0e7bc7269976
ryuujo1573 <me@ryuujo.com>
2022-10-15 21:20:53 +0800

    🧱 Progress!

--- src/components/resume.tsx
+++ src/components/resume.tsx
@@ -95,15 +95,16 @@ const Specialties = styled.div<{ radius: string }>`
   /* box-sizing: content-box; */
   align-items: center;
   justify-content: center;
+  --radius: ${p => p.radius};
   &>* {
     display: flex;
     align-items: center;
     justify-content: center;
 
     border: #ccc solid 1px;
-    border-radius: ${props => props.radius};
-    width: calc(2 * ${props => props.radius});
-    height: calc(2 * ${props => props.radius});
+    border-radius: var(--radius);
+    width: calc(2 * var(--radius));
+    height: calc(2 * var(--radius));
 
     margin-inline: -.6ch;
     box-sizing: border-box;
@@ -113,12 +114,112 @@ const Specialties = styled.div<{ radius: string }>`
     &:hover {
       border-color: #666;
     }
-    &::after {
-      content: 'aaa',
-      
-    }
   }
 `
+const LanguageSkill = styled.div<{ radius: string, innerRadius?: string }>`
+  --radius: ${p => p.radius};
+  --border-color: #eee;
+  @property --percentage {
+    syntax: '<percentage>';
+    inherits: false;
+    initial-value: 0%;
+  }
+
+  @keyframes circular-anim {
+    from {--percentage: 0%;} to {--percentage: 100%;}
+  }
+
+  display: flex;
+  /* box-sizing: content-box; */
+  align-items: center;
+  justify-content: space-between;
+
+  position: relative;
+  margin-bottom: 2em;
+  
+  &>* {
+    --percentage: 0%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    z-index: 0;
+    
+    border: #eee solid 1px;
+    // https://stackoverflow.com/questions/71860699/is-there-any-way-to-animate-a-conic-gradient-in-css
+    // https://dev.to/afif/we-can-finally-animate-css-gradient-kdk
+    background: conic-gradient(
+      #6cf calc(var(--percentage) * var(--score) / var(--score-max)),
+      #fff calc(var(--percentage) * var(--score) / var(--score-max))
+    );
+
+    box-sizing: border-box;
+    border-radius: var(--radius);
+    width: calc(2 * var(--radius));
+    height: calc(2 * var(--radius));
+
+    margin-inline: 0ch;
+
+    padding: 5%;
+    animation: circular-anim cubic-bezier(0.175, 0.885, 0.32, 1.2) 1s forwards;
+    transition: all cubic-bezier(0.165, 0.84, 0.44, 1) 1s;
+    border-color: var(--border-color);
+    &:hover {
+      --border-color: #66ccffab;      
+    }
+
+    position: relative;
+
+    &::before {
+      content: '';
+      display: block;
+      position: absolute;
+      width: calc(2 * ${p => p.innerRadius ?? p.radius + ' - 20px'});
+      height: calc(2 * ${p => p.innerRadius ?? p.radius + ' - 20px'});
+      border: var(--border-color) solid 1px;
+      border-radius: calc(var(--radius) - 10px);
+      z-index: -1;
+      background-color: var(--page-color, #fff);
+      /* translate: ; */
+      transition: all cubic-bezier(0.165, 0.84, 0.44, 1) 1s;
+      border-color: var(--border-color);
+    }
+  }
+
+  &>*::after {
+    position: absolute;
+    top: 100%;
+    z-index: 10;
+    opacity: 0;
+    content: ' ';
+    line-height: 2;
+    transition: opacity cubic-bezier(0.165, 0.84, 0.44, 1) 1s;
+  }
+  &>*:hover::after {
+    counter-reset: score var(--score) scoreMax var(--score-max);
+    content: counter(score)" / "counter(scoreMax);
+    opacity: 1;
+  }
+`
+
+const Hinted = styled.div<{ value: string, }>`
+  position: relative;
+  &::after {
+    content: '${p => p.value}';
+    position: absolute;
+    /* display: none; */
+    width: fit-content;
+    top: calc(100% + 0.5em);
+    opacity: 0;
+    transition: all cubic-bezier(0.165, 0.84, 0.44, 1) 1s;
+    
+  }
+  &:hover::after {
+      /* display: block; */
+      opacity: 1;
+  }
+`
+
 
 export interface IResumeProps {
   name: string,
@@ -144,29 +245,37 @@ export default class Resume extends React.PureComponent<IResumeProps> {
               <Subtitle>{this.props.nickname}</Subtitle>
             </div>
             <div>
-              <p>me@ryuujo.com</p>
-              <p>17861068979</p>
-              <p><a href='https://github.com/ryuujo1573'>ryuujo1573</a></p>
+              <p>{this.props.email}</p>
+              <p>{this.props.phone}</p>
+              <p><a href={'https://github.com/' + this.props.githubId}>{this.props.githubId}</a></p>
             </div>
             <Spacer />
             <Avatar />
           </Header>
-          <Row style={{ flex: 1}}>
+          <Row style={{ flex: 1 }}>
             <SideColumn style={{ backgroundColor: '', flex: 1, }}>
               <Section children="PERSONAL SPECIALTIES" />
               <Specialties radius="4ch">
-                <div><IconIdea /></div>
-                <div><IconWindows /></div>
-                <div><IconServer /></div>
+                <Hinted value='设计'><IconIdea /></Hinted>
+                <Hinted value='前端'><IconWindows /></Hinted>
+                <Hinted value='后端'><IconServer /></Hinted>
               </Specialties>
 
               <br />
-              <Section children="PERSONAL EXPERIENCES" />
+              <Section children="LANGUAGE SKILLS" />
+              <LanguageSkill radius="5ch" innerRadius='3.5ch'>
+                <div style={{'--score': 525, '--score-max': 710}}>CET-6</div>
+                <div style={{'--score': 85, '--score-max': 100}}>CJT-6</div>
+              </LanguageSkill>
+              <li>
+                
+              </li>
+              <Section children="LANGUAGE SKILLS" />
 
               <Spacer />
             </SideColumn>
-            <Column style={{flex: 1, marginLeft: '5%',}}>
-              <Section children="PERSONAL EXPERIENCE"/>
+            <Column style={{ flex: 1, marginLeft: '5%', }}>
+              <Section children="PERSONAL EXPERIENCE" />
               <Spacer />
               <Section children="PROJECTS" />
               <Spacer />
